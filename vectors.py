b274741833ef463c8c39a4e5783a5a361276b2dd
Drilon Kllokoqi <drilonkllokoqi@hotmail.com>
2022-11-30 22:10:01 +0100

    vectors

--- vectors.py
+++ vectors.py
@@ -1,12 +1,20 @@
 """Vectors."""
 
+
 import numpy as np
-from attrs import field, validate, define, validators
+from attrs import field, define, validators
 from attrs.validators import instance_of
 
+
 @define
-class vector2d:
+class Vector2D:
     x: float = field(validators=instance_of(float))
     y: float = field(validators=instance_of(float))
 
-    
+    def __str__(self):
+        """Human-readable string representation of the vector."""
+        return f'{self.x:g}i + {self.y:g}j'
+
+
+vec = Vector2D(x=4.3, y=8.8)
+print(vec)
